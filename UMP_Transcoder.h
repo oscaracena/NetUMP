40a34064fb6c63eb11b45402baae7f549d2a7223
bbouchez <31064337+bbouchez@users.noreply.github.com>
2024-12-23 08:58:54 +0100

    Source code modified to C in place of C++

--- UMP_Transcoder.h
+++ UMP_Transcoder.h
@@ -1,9 +1,9 @@
 /*
  *  UMP_Transcoder.h
- *  Functions to convert UMP <-> MIDI 1.0 for rtpUMP
-  * SYSEX packet format is based on MMA Prop 073
+ *  Functions to convert UMP <-> MIDI 1.0
+ *  @231224
  *
- * Copyright (c) 2023 Benoit BOUCHEZ / KissBox
+ * Copyright (c) 2022 - 2024 Benoit BOUCHEZ / KissBox
  * License : MIT
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -31,26 +31,33 @@
 
 #include <stdint.h>
 
-#define MAX_SYSEX_SIZE		1024
+// This value covers most of MIDI applications. Redefine it if needed
+#ifndef MAX_SYSEX_SIZE
+#define MAX_SYSEX_SIZE		256
+#endif
 
 //! Control structure to decode UMP SYSEX 7-bit packets into MIDI 1.0 SYSEX
 typedef struct {
-	bool UMPStarted;		// Avoid processing SYSEX Continue or SYSEX End if a SYSEX Start has not been received
+	unsigned char UMPStarted;		// Avoid processing SYSEX Continue or SYSEX End if a SYSEX Start has not been received
 	unsigned int SYSEXSize;	// Size of rebuilt SYSEX packet
 	uint8_t SYSEXBuffer[MAX_SYSEX_SIZE];
 } TSYSEX7_Decoder_Control;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 //! Transform a MIDI 1.0 message into a single UMP message
 //! \param MIDIBytes : pointer to MIDI 1.0 message to convert (includes SYSEX up to 8 bytes including F0 and F7)
 //! \param MIDI1Length : number of bytes in the MIDI 1.0 message
 //! \param UMPMessage : array of 4 uint32_t to receive the converted message
 //! \return true if the MIDI message can be converted in a single UMP packet. False means message can not be converted into single UMP (or message is not recognized)
-bool TranscodeMIDI1_UMP (uint8_t* MIDIBytes, unsigned int MIDI1Length, uint32_t* UMPMessage);
+unsigned char TranscodeMIDI1_UMP (uint8_t* MIDIBytes, unsigned int MIDI1Length, uint32_t* UMPMessage);
 
 //! Transform a MIDI 1.0 SYSEX packet into a UMP stream. The function must be called as long it returns true to get the UMP sequence
 //! \param PtrSYSEX Must be zeroed before the first call for each new SYSEX to process. Contains position of the next byte in the SYSEX payload
 //! \return true if a UMP packet has been generated (packet can be sent). False means no more data to be converted to UMP (or this is not a valid SYSEX)
-bool TranscodeSYSEX_UMP(uint8_t* MIDIBytes, unsigned int MIDI1Length, unsigned int* PtrSYSEX, uint32_t* UMPMessage);
+unsigned char TranscodeSYSEX_UMP(uint8_t* MIDIBytes, unsigned int MIDI1Length, unsigned int* PtrSYSEX, uint32_t* UMPMessage);
 
 //! Transform UMP message into MIDI 1.0 equivalent
 //! \param MIDIMsg array of bytes to receive the MIDI 1.0 message from UMP. Array MUST be at least 8 bytes long, as this function decodes MT=3 Single SYSEX packet (8 bytes SYSEX)
@@ -60,5 +67,9 @@ unsigned int TranscodeUMP_MIDI1 (uint32_t* SourceUMP, uint8_t* MIDIMsg);
 //! Process UMP MT=3 packet to rebuild MIDI 1.0 SYSEX 
 //! \return 0 if no SYSEX data is available, size of SYSEX packet if a SYSEX has been successfully rebuilt
 unsigned int RebuildSYSEXFromUMP(uint32_t* SourceUMP, TSYSEX7_Decoder_Control* Decoder);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
